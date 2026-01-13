91b19fd2846ec64dc0afd2d31d68d31ee99bb851
januarcexp <mccoystar1818@gmail.com>
2026-01-13 21:14:54 +0700

    update

--- tests/ts_003.spec.ts
+++ tests/ts_003.spec.ts
@@ -4,7 +4,7 @@ test.use({
   storageState: 'playwright/.auth/pelanggan.json'
 });
 
-test('test', async ({ page }) => {
+test('update dengan main', async ({ page }) => {
   await page.goto('http://localhost/plywood/public/');
   await page.getByRole('link', { name: 'Pemesanan' }).click();
   await page.getByRole('tab', { name: 'Selesai' }).click();
